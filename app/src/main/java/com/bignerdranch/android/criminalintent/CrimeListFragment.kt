83ee7bf9919f69c05f98a0c03804e215c90be4d5
cjgunnar <34259593+cjgunnar@users.noreply.github.com>
2020-03-13 11:51:22 -0500

    add chap 11 w/challenge

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,16 +19,12 @@ private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
 
-    private lateinit var crimeRecyclerView : RecyclerView
+    private lateinit var crimeRecyclerView: RecyclerView
 
-    private var adapter : CrimeAdapter? = null
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
     }
 
     override fun onCreateView(
@@ -35,53 +32,66 @@ class CrimeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
 
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view)
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
-
+        crimeRecyclerView.adapter = adapter
         return view
     }
 
-    private inner class CrimeHolder(view : View) : RecyclerView.ViewHolder(view), View.OnClickListener {
-        private val titleTextView : TextView = view.findViewById(R.id.crime_title)
-        private val dateTextView : TextView = view.findViewById(R.id.crime_date)
-        private val isSolvedImageView : ImageView = view.findViewById(R.id.crime_solved)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+        private val titleTextView: TextView = view.findViewById(R.id.crime_title)
+        private val dateTextView: TextView = view.findViewById(R.id.crime_date)
+        private val isSolvedImageView: ImageView = view.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
         }
 
-        fun bind(crime : Crime) {
+        fun bind(crime: Crime) {
             titleTextView.text = crime.title
             dateTextView.text = DateFormat.getDateInstance(DateFormat.MEDIUM).format(crime.date)
-            isSolvedImageView.visibility = if(crime.isSolved) View.VISIBLE else View.INVISIBLE
+            isSolvedImageView.visibility = if (crime.isSolved) View.VISIBLE else View.INVISIBLE
         }
 
         override fun onClick(p0: View?) {
             val msg = getString(R.string.clicked_item_view_toast).format(titleTextView.text)
-            Toast.makeText(context, msg,Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
         }
     }
 
-    private inner class CrimeAdapter(var crimes : List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
 
         val minorCrime = 0
         val seriousCrime = 1
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-            fun minorCrimeViewHolder() : CrimeHolder {
+            fun minorCrimeViewHolder(): CrimeHolder {
                 val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
                 return CrimeHolder(view)
             }
-            fun seriousCrimeViewHolder() : CrimeHolder {
+
+            fun seriousCrimeViewHolder(): CrimeHolder {
                 val view = layoutInflater.inflate(R.layout.list_item_serious_crime, parent, false)
                 return CrimeHolder(view)
             }
 
-            return if(viewType == minorCrime) minorCrimeViewHolder()
+            return if (viewType == minorCrime) minorCrimeViewHolder()
             else seriousCrimeViewHolder()
         }
 
@@ -93,12 +103,11 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun getItemViewType(position: Int): Int {
-            return if(crimes[position].requiresPolice) seriousCrime else minorCrime
+            return minorCrime //TODO FIX AND REIMPLEMENT if (crimes[position].requiresPolice) seriousCrime else minorCrime
         }
     }
 
-    private fun updateUI() {
-        val crimes = crimeListViewModel.crimes
+    private fun updateUI(crimes : List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -3,15 +3,8 @@ package com.bignerdranch.android.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
 
-    init {
-        for(i in 0..100) {
-            val crime = Crime()
-            crime.title = "Crime $i"
-            if(i % 11 == 0) {crime.requiresPolice = true; crime.title += " SERIOUS CRIME"}
-            if(i % 3 == 0) crime.isSolved = true
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
 }
